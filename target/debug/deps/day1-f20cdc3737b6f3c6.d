d340cd76c1842e28503276e529b7a9a46b021a04
Stefano Cefalu <stcefalu@protonmail.com>
2021-11-27 19:46:14 -0800

    Downgrade to stable rust v1.56.1

---
+++ target/debug/deps/day1-f20cdc3737b6f3c6.d
@@ -0,0 +1,5 @@
+/home/mnohem/Code/aoc2021/target/debug/deps/day1-f20cdc3737b6f3c6: day1/src/main.rs
+
+/home/mnohem/Code/aoc2021/target/debug/deps/day1-f20cdc3737b6f3c6.d: day1/src/main.rs
+
+day1/src/main.rs:
---
+++ target/debug/deps/helper-a5f849422b29e643.d
@@ -0,0 +1,7 @@
+/home/mnohem/Code/aoc2021/target/debug/deps/helper-a5f849422b29e643.rmeta: helper/src/lib.rs
+
+/home/mnohem/Code/aoc2021/target/debug/deps/libhelper-a5f849422b29e643.rlib: helper/src/lib.rs
+
+/home/mnohem/Code/aoc2021/target/debug/deps/helper-a5f849422b29e643.d: helper/src/lib.rs
+
+helper/src/lib.rs:
