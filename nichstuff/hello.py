f8ef8204d8778b97d78e024d8bff8e276ce8ce02
Mnohem <69613082+Mnohem@users.noreply.github.com>
2021-12-01 00:19:32 -0800

    Revert "Just a few bug fixes, nothing special"

--- nichstuff/hello.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-if __name__ == "__main__":
-    print("Killer Fish")
-    input()
-    print("Killer Fish from San Diego!")
-    input()
-    print("I'll be your Killer Fish for the evening!")
