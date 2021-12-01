133864bc0cb2a8c6f073c0d30b8bf15f0be50512
Stefano Cefalu <stcefalu@protonmail.com>
2021-11-30 21:44:41 -0800

    Completed day1 + fixed template

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,8 +1,8 @@
-use helper::easy_input;
-use std::env::args;
-
 fn main() {
     let input = {
+        use helper::easy_input;
+        use std::env::args;
+
         let run_arg = args().next().unwrap();
         let day_num = run_arg.split('/').last().unwrap();
         easy_input(day_num)
