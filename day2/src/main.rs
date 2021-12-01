dc6311b152dea2ad0150d28e5dd95c1cdcb998f6
Stefano Cefalu <stcefalu@protonmail.com>
2021-11-30 21:52:03 -0800

    fixed day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let input = {
+        use helper::easy_input;
+        use std::env::args;
+
+        let run_arg = args().next().unwrap();
+        let day_num = run_arg.split('/').last().unwrap();
+        easy_input(day_num)
+    };
+
+    println!("{}", input);
 }
