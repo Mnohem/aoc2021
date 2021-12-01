91cef70f991ccb924d4e4b3afec45adf553450e8
Nicholas Tsimerekis <ntsimerekis@gmail.com>
2021-12-01 00:13:22 -0800

    even more bug fixes, wow this code is so buggy

---
+++ nichstuff/fish.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("killer fish.\n");
+  getchar();
+  printf("KILLER FISH FROM SAN DIEGO!\n");
+  getchar();
+  printf("I don't know what I am, but I taste really good!\n");
+  getchar();
+  printf("I'm a killer fish!\n");
+  getchar();
+  printf("Hello\n");
+  getchar();
+  printf("I'll be your killer fish for the evening\n");
+  getchar();
+  printf("Thank you!\n");
+}
