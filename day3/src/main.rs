f6496c5d8cab9cb843b6d3526edce3fea33c8081
Stefano Cefalu <stcefalu@protonmail.com>
2021-12-03 14:35:24 -0800

    day3 finally finished

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -7,9 +7,55 @@ fn main() {
         easy_input(run_arg.split('/').last().unwrap())
     };
 
-    //Solution starts here
-    let fold = input.lines().fold([0; 12], |mut acc, st| {
-        for (i, c) in st.bytes().enumerate() {
+    day3_part1(&input);
+    day3_part2(&input);
+}
+
+fn day3_part2(input: &str) {
+    let mut valid_nums_o2: Vec<_> = input.lines().collect();
+    let mut bits = common_bits(input.lines());
+    let mut i = 0;
+    let o2 = loop {
+        valid_nums_o2.retain(|x| {
+            let bit = x.bytes().nth(i).unwrap() - b'0';
+            !((bits[i] >= 0) ^ (bit == 1))
+        });
+        if valid_nums_o2.len() == 1 {
+            break u32::from_str_radix(&valid_nums_o2[0], 2).unwrap();
+        } else if valid_nums_o2.is_empty() {
+            panic!("No valid nums");
+        }
+        bits = common_bits(valid_nums_o2.iter());
+        i += 1;
+    };
+
+    let mut valid_nums_co2: Vec<_> = input.lines().collect();
+    bits = common_bits(input.lines());
+    i = 0;
+    let co2 = loop {
+        valid_nums_co2.retain(|x| {
+            let bit = x.bytes().nth(i).unwrap() - b'0';
+            (bits[i] >= 0) ^ (bit == 1)
+        });
+        if valid_nums_co2.len() == 1 {
+            break u32::from_str_radix(&valid_nums_co2[0], 2).unwrap();
+        } else if valid_nums_co2.is_empty() {
+            panic!("No valid nums");
+        }
+        bits = common_bits(valid_nums_co2.iter());
+        i += 1;
+    };
+
+    println!("{}", co2 * o2);
+}
+
+fn common_bits<'a, SI, T>(lines: SI) -> [i32; 12]
+where
+    SI: Iterator<Item = T>,
+    T: std::string::ToString,
+{
+    let bits = lines.fold([0; 12], |mut acc, st| {
+        for (i, c) in st.to_string().bytes().enumerate() {
             if c == b'1' {
                 acc[i] += 1i32;
             } else if c == b'0' {
@@ -18,15 +64,17 @@ fn main() {
         }
         acc
     });
+    bits
+}
 
-    let gamma: u32 = fold
+fn day3_part1(input: &str) {
+    let mut bits = common_bits(input.lines());
+    bits.reverse();
+    let gamma: u32 = bits
         .iter()
         .enumerate()
         .map(|(i, &x)| (x.is_positive() as u32) << i)
         .sum();
     let epsilon = !gamma & 0xFFF;
-
-    println!("{:012b}", gamma);
-    println!("{:012b}", epsilon);
     println!("{:?}", gamma * epsilon);
 }
