7f3cf9496388b0f7bf1814e873cd28e7d9d38e3e
Stefano Cefalu <stcefalu@protonmail.com>
2021-12-02 18:48:29 -0800

    finally made easy_input less path specific

--- helper/src/lib.rs
+++ helper/src/lib.rs
@@ -1,8 +1,25 @@
 pub fn easy_input(input_name: &str) -> String {
+    use std::fs::read_to_string;
+    use std::path::Path;
     //input file should be the same name as crate for aoc
     //so crate "day1" should have input file name be "day1"
-    std::fs::read_to_string(format!("/home/mnohem/Code/aoc2021/input/{}", input_name))
-        .expect("Could not read input file").trim().to_owned()
+    let input_name = "/input/".to_owned() + input_name;
+
+    let input_path = Path::new(".")
+        .canonicalize()
+        .unwrap()
+        .ancestors()
+        .filter(|path| path.to_str().unwrap().ends_with("aoc2021"))
+        .next()
+        .unwrap()
+        .to_string_lossy()
+        .to_string()
+        + &input_name;
+
+    read_to_string(input_path)
+        .expect("Could not read input file")
+        .trim()
+        .to_owned()
 }
 
 #[cfg(test)]
